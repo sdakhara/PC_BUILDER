9abfae352d98a54525ac45e4093d869b416b449e
Sujal Dankhara <scdakhara123@gmail.com>
2021-08-02 15:23:57 +0530

    backend connection wwith front end

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return "<h1>hello</h1>"
+def index():
+    return  render_template('index.html')
 
 
 if __name__ == '__main__':
