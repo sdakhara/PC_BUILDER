1c0c04271a9b9756f4c40373044a876643cacb7d
Sujal Dankhara <scdakhara123@gmail.com>
2021-08-04 11:35:14 +0530

    reformation

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ url = "https://cpu-data.p.rapidapi.com/cpus"
 headers = {
     'x-rapidapi-key': "1c2a41ba92mshec9d61014ca2e68p1e3f1cjsn84607970fe3a",
     'x-rapidapi-host': "cpu-data.p.rapidapi.com"
-    }
+}
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return  render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/cpu')
 def cpu():
     response = requests.request("GET", url, headers=headers)
 
-
     return jsonify(response.text)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
