a5218096cb48f4a7fbce9930d57ac7b2833cc321
Sujal Dankhara <scdakhara123@gmail.com>
2021-08-08 21:04:59 +0530

    cpu data refactor

--- cpuDataRapidApi.py
+++ cpuDataRapidApi.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.types import String
-# mysql+pymysql://root:@127.0.0.1:3306/the_gym
 
 url = "https://cpu-data.p.rapidapi.com/cpus"
 
