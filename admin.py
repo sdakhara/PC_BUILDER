9993d433e8a209b654bd41f29c607e6e2c696b12
Sujal Dankhara <scdakhara123@gmail.com>
2021-08-09 02:51:35 +0530

    layout setup with the route added for the each template

--- admin.py
+++ admin.py
@@ -8,5 +8,30 @@ def home():
     return render_template('Admin/index.html')
 
 
+@app.route('/messages')
+def messages():
+    return render_template('Admin/messages.html')
+
+
+@app.route('/users')
+def users():
+    return render_template('Admin/users.html')
+
+
+@app.route('/inventory')
+def inventory():
+    return render_template('Admin/inventory.html')
+
+
+@app.route('/statistics')
+def statistics():
+    return render_template('Admin/statistics.html')
+
+
+@app.route('/system')
+def system():
+    return render_template('Admin/system.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
