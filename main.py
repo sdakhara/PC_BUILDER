352f2282aaa9ad356a5c4ddc1dbb3d20b9f874fd
Sujal Dankhara <scdakhara123@gmail.com>
2021-08-09 00:47:02 +0530

    trash commit

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# User Routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('User/index.html')
 
 
 if __name__ == '__main__':
