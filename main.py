90f62090ab02e082fbe7372dd53fbf336fd22da2
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2021-10-16 12:06:04 +0530

    updated

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from logics.user.autobuild import logic
+# from logics.user.autobuild import logic
 
 
 app = Flask(__name__)
 
-logic = logic()
+# logic = logic()
 
 
 # User Routes
@@ -13,9 +13,9 @@ def index():
     return render_template('User/index.html')
 
 
-@app.route('/test/<int:budget>')
-def test(budget):
-    return render_template('User/testHTMLs/logictest.html', data=logic.buildpc(budget))
+# @app.route('/test/<int:budget>')
+# def test(budget):
+#     return render_template('User/testHTMLs/logictest.html', data=logic.buildpc(budget))
 
 
 if __name__ == '__main__':
