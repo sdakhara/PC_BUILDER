19e05589654171ca607c02a94622436734e27764
Sujal Dankhara <scdakhara123@gmail.com>
2022-04-05 18:10:11 +0530

    admin work: login activity for admin with login log is added.

--- admin.py
+++ admin.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from logics.admin.IPLocation import get_ip
-from logics.admin.datashare import userdt
+from logics.admin.datashare import userdt, Authentication
 
 app = Flask(__name__)
 usrdata = userdt()
+verifier = Authentication()
 # visitor counter
 
 counter = 0
@@ -31,10 +32,48 @@ def getIP():
     country = get_ip(request.remote_addr)
     return jsonify(country)
 
-@app.route('/')
-def home():
-    return render_template('Admin/index.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    from sqlalchemy import create_engine, Column
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.types import String, Integer
+
+    engine = create_engine("mysql+pymysql://Sujal:9099@127.0.0.1:3306/testdb")
+    Session = sessionmaker()
+    db = Session(bind=engine)
+    Base = declarative_base()
+
+    class adminloginrecord(Base):
+        __tablename__ = 'adminloginrecord'
+
+        RID = Column(Integer, primary_key=True, autoincrement=True)
+        AdminID = Column(String)
+        AdminName = Column(String)
+        LoginTime = Column(String)
+
+    class admindata(Base):
+        __tablename__ = 'admindata'
+
+        AdminID = Column(String, primary_key=True)
+        AdminName = Column(String)
+        Email = Column(String)
+        Password = Column(String)
+        PhoneNo = Column(String)
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        data = verifier.verify(email, password)
+        if data:
+            return redirect(url_for('dashboard'))
+
+    return render_template('Admin/login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('Admin/index.html')
 
 @app.route('/messages')
 def messages():
@@ -43,7 +82,8 @@ def messages():
 
 @app.route('/users')
 def users():
-    dt = usrdata.getAllUser()
+    # dt = usrdata.getAllUser()
+    dt = verifier.verify()
     return render_template('Admin/users.html', dt=dt)
 
 
--- logics/Admin/datashare.py
+++ logics/Admin/datashare.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 from sqlalchemy import create_engine, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.types import Integer, String, Float
+from sqlalchemy.types import String, Integer
 
 engine = create_engine("mysql+pymysql://Sujal:9099@127.0.0.1:3306/testdb")
 Session = sessionmaker()
 db = Session(bind=engine)
 Base = declarative_base()
+
+
 class userdata(Base):
     __tablename__ = 'userdata'
 
@@ -17,6 +20,36 @@ class userdata(Base):
     PhoneNo = Column(String)
 
 
+class adminloginrecord(Base):
+    __tablename__ = 'adminloginrecord'
+
+    RID = Column(Integer, primary_key=True, autoincrement=True)
+    AdminID = Column(String)
+    AdminName = Column(String)
+    LoginTime = Column(String)
+
+
+class admindata(Base):
+    __tablename__ = 'admindata'
+
+    AdminID = Column(String, primary_key=True)
+    AdminName = Column(String)
+    Email = Column(String)
+    Password = Column(String)
+    PhoneNo = Column(String)
+
+
 class userdt:
     def getAllUser(self):
         return db.query(userdata).all()
+
+
+class Authentication:
+    def verify(self, email, password):
+        data = db.query(admindata).all()
+        for dt in data:
+            if dt.Email == email and dt.Password == password:
+                addLog = adminloginrecord(AdminName=dt.AdminName, AdminID=dt.AdminID, LoginTime=datetime.now())
+                db.add(addLog)
+                db.commit()
+                return True
