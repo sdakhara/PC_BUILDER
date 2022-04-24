33d4b6b9e9026a7aa8c767eb05e6230806015403
JayBhatt-github <84282240+JayBhatt-github@users.noreply.github.com>
2022-04-24 14:52:30 +0530

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-from logics.admin.datashare import Authentication
+from logics.Admin.datashare import Authentication
 from logics.user.autobuild import logic
 from logics.user.iplogics import ipControl
 
@@ -199,22 +199,6 @@ def secondhandpc():
 
     return render_template('User/secondhandpc.html')
 
-
-
-
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    budget = 0
-    cpu = 0
-    ram = 0
-    hdd = 0
-    if request.method == 'POST':
-        budget = request.form.get('budget')
-        cpu = request.form.get('CPU')
-        ram = request.form.get('RAM')
-        hdd = request.form.get('HDD')
-    return render_template('User/testHTMLs/logictest.html',
-                           data=logic.buildpc(int(budget), bool(cpu), bool(ram), bool(hdd)))
 
 
 if __name__ == '__main__':
